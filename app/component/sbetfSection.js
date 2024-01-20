7819d8e490246941c5fd491e139217166c57f263
Abhi <abhippateltx@gmail.com>
2024-01-20 15:27:33 +0530

    accordian

--- app/component/sbetfSection.js
+++ app/component/sbetfSection.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import Btn from './button'
 
+
+
+const accordionData = [
+  {
+    title: 'Blackrock ETF Trading Rewards',
+    content: 'SBETF will invest part of its token supply in the popular Spot Bitcoin ETFs. The profits from this investment will be shared with the community through staking rewards and other programs. This makes SBETF the first token to invest in a Bitcoin ETF and give back to its community.',
+  },
+  {
+    title: 'Exclusive NFT Collection and Marketplace',
+    content: 'The NFTs will be created with popular meme artists to showcase their creativity, so you can own a piece of digital art that\'s truly unique. The NFTs will be released in batches, with each batch featuring a different theme. The first batch of NFTs will be released in Q2 2024. SBETF introduces an exclusive ecosystem and marketplace for such NFTs.',
+  },
+  {
+    title: 'Staking and Rewards',
+    content: 'Participate in the SBETF ecosystem by staking your tokens and earn rewards. These rewards will be used to buy more tokens, creating a sustainable and thriving community.',
+  },
+  {
+    title: 'Meme GPT',
+    content: 'Our homegrown gen AI model to build your own meme. Our token holders will get early access to this tool.',
+  },
+];
+
 function SbetfSection() {
   return (
     <div>
@@ -26,18 +47,17 @@ function SbetfSection() {
                         className="crumina-module crumina-accordion accordion--style5"
                         id="accordion6"
                       >
-                        <li className="accordion-panel">
+                      {accordionData.map((item, index) => (
+                        <li className="accordion-panel" key={index}>
                           <div className="panel-heading">
                             <a
-                              href="#toggleSample6"
+                              href={`#toggle${index}`}
                               className="accordion-heading collapsed"
                               data-toggle="collapse"
                               data-parent="#accordion"
                               aria-expanded="false"
                             >
-                              <span className="title">
-                                Blackrock ETF Trading Rewards :
-                              </span>
+                              <span className="title">{item.title}</span>
                               <span className="icons">
                                 <svg className="betf-icon icon-arrow-bottom">
                                   <use xlinkHref="#icon-arrow-bottom" />
@@ -46,121 +66,15 @@ function SbetfSection() {
                             </a>
                           </div>
                           <div
-                            id="toggleSample6"
-                            className="panel-collapse collapse in"
+                            id={`toggle${index}`}
+                            className={`panel-collapse collapse ${index === 0 ? 'in' : ''}`}
                             aria-expanded="false"
                             role="tree"
                           >
-                            <div className="panel-info ">
-                              SBETF will invest part of its token supply in the
-                              popular Spot Bitcoin ETFs. The profits from this
-                              investment will be shared with the community
-                              through staking rewards and other programs. This
-                              makes SBETF the first token to invest in a Bitcoin
-                              ETF and give back to its community.
-                            </div>
+                            <div className="panel-info">{item.content}</div>
                           </div>
                         </li>
-                        <li className="accordion-panel">
-                          <div className="panel-heading">
-                            <a
-                              href="#toggleOne6"
-                              className="accordion-heading collapsed"
-                              data-toggle="collapse"
-                              data-parent="#accordion"
-                              aria-expanded="false"
-                            >
-                              <span className="title">
-                                Exclusive NFT Collection and Marketplace:
-                              </span>
-                              <span className="icons">
-                                <svg className="betf-icon icon-arrow-bottom">
-                                  <use xlinkHref="#icon-arrow-bottom" />
-                                </svg>
-                              </span>
-                            </a>
-                          </div>
-                          <div
-                            id="toggleOne6"
-                            className="panel-collapse collapse"
-                            aria-expanded="false"
-                            role="tree"
-                          >
-                            <div className="panel-info">
-                              The NFTs will be created with popular meme artists
-                              to showcase their creativity, so you can own a
-                              piece of digital art that's truly unique. The NFTs
-                              will be released in batches, with each batch
-                              featuring a different theme. The first batch of
-                              NFTs will be released in Q2 2024. SBETF introduces
-                              an exclusive ecosystem and marketplace for such
-                              NFTs.
-                            </div>
-                          </div>
-                        </li>
-                        <li className="accordion-panel">
-                          <div className="panel-heading">
-                            <a
-                              href="#toggleTwo6"
-                              className="accordion-heading collapsed"
-                              data-toggle="collapse"
-                              data-parent="#accordion"
-                              aria-expanded="false"
-                            >
-                              <span className="title">
-                                Staking and Rewards :
-                              </span>
-                              <span className="icons">
-                                <svg className="betf-icon icon-arrow-bottom">
-                                  <use xlinkHref="#icon-arrow-bottom" />
-                                </svg>
-                              </span>
-                            </a>
-                          </div>
-                          <div
-                            id="toggleTwo6"
-                            className="panel-collapse collapse"
-                            aria-expanded="false"
-                            role="tree"
-                          >
-                            <div className="panel-info">
-                              Participate in the SBETF ecosystem by staking your
-                              tokens and earn rewards. These rewards will be
-                              used to buy more tokens, creating a sustainable
-                              and thriving community..
-                            </div>
-                          </div>
-                        </li>
-                        <li className="accordion-panel">
-                          <div className="panel-heading">
-                            <a
-                              href="#toggleThree6"
-                              className="accordion-heading collapsed"
-                              data-toggle="collapse"
-                              data-parent="#accordion"
-                              aria-expanded="false"
-                            >
-                              <span className="title">Meme GPT:</span>
-                              <span className="icons">
-                                <svg className="betf-icon icon-arrow-bottom">
-                                  <use xlinkHref="#icon-arrow-bottom" />
-                                </svg>
-                              </span>
-                            </a>
-                          </div>
-                          <div
-                            id="toggleThree6"
-                            className="panel-collapse collapse"
-                            aria-expanded="false"
-                            role="tree"
-                          >
-                            <div className="panel-info">
-                              Our homegrown gen AI model to build your own meme.
-                              Our token holders will get early access to this
-                              tool.
-                            </div>
-                          </div>
-                        </li>
+                      ))}
                       </ul>
                     </div>
                     <div className=" mt60">
